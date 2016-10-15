79daf8f4d544cb718f3a294b036f3af2b31fb80e
Nate Blair <nategribble@gmail.com>
2016-10-15 13:07:34 -0600

    Basics of asteroids

---
+++ MoonMiningMania/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+    public float asteroidVelocity;
+    public float startingAngle;
+
+    Rigidbody2D myRigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        float radians = (Mathf.PI * startingAngle / 180);
+        myRigidBody = this.GetComponent<Rigidbody2D>();
+        myRigidBody.velocity = new Vector3(Mathf.Cos(radians) * asteroidVelocity, (Mathf.Sin(radians) * asteroidVelocity), 0);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
